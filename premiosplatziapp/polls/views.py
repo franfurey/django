528422ccc3662368842a833d4c46310baa8e3958
Francisco Furey <franciscofurey@gmail.com>
2023-03-24 14:22:14 -0300

    commit

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse('Hola Fran')
+    return HttpResponse('Estas en la pagina principal de premios Platzi App')
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
 
