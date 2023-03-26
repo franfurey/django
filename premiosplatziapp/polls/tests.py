48c35473e8f976fcd315e7756a13a590a0b15dd0
Francisco Furey <franciscofurey@gmail.com>
2023-03-26 18:12:09 -0300

    commit

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.utils import timezone
+import datetime
+from .models import Question
 
 # Create your tests here.
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_questions(self):
+        """was_published_recently returns False for questions whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days = 30)
+        future_question = Question(question_text = "Quien es el mejor CD de Platzi?" ,pub_date = time)
+        self.assertIs(future_question.was_published_recently(), False)
